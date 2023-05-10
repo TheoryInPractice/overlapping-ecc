bcd911165e0cb7b832fc5bb14e4fe24d3beef94a
Alex Crane <crane@cs.utah.edu>
2023-05-10 16:43:25 -0600

    chpc setup

--- Exp1-Algorithm-Evaluation/Run_RECC_Algorithms.jl
+++ Exp1-Algorithm-Evaluation/Run_RECC_Algorithms.jl
@@ -3,9 +3,9 @@ using MAT
 include("../src/RECCAlgs.jl")
 include("../src/EdgeCatClusAlgs.jl")
 
-datasets = ["Brain", "MAG-10", "Cooking", "DAWN"]
+# datasets = ["Brain", "MAG-10", "Cooking", "DAWN"]
 # datasets = ["Walmart-Trips"]
-# datasets = ["Trivago"]
+datasets = ["Trivago"]
 
 deletion_budgets = [0, .01, .05, .1, .15, .2, .25]
 numdata = length(datasets)
