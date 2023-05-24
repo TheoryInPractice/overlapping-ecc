90af1a9533e23684dd335499da323225b23672ab
Alex <crane@cs.utah.edu>
2023-05-24 10:34:58 -0600

    updated greedy comparison code - transferring to CADE

--- Exp1-Algorithm-Evaluation/Greedy_Comparison.jl
+++ Exp1-Algorithm-Evaluation/Greedy_Comparison.jl
@@ -2,13 +2,18 @@ using MAT
 using JLD
 include("../src/EdgeCatClusAlgs.jl")
 include("../src/helpers.jl")
+include("../src/GoECCAlgs.jl")
+include("../src/LoECCAlgs.jl")
 
 # GoECC_Greedy_Compare()
 
 function LoECC_Greedy_Compare()
 
-    datasets = ["Brain", "MAG-10", "Cooking", "DAWN", "Walmart-Trips", "Trivago"]
+    println("LO-ECC")
+
+    # datasets = ["Brain", "MAG-10", "Cooking", "DAWN", "Walmart-Trips", "Trivago"]
     # datasets = ["Brain", "MAG-10", "Cooking", "DAWN", "Walmart-Trips"]
+    datasets = ["Trivago"]
 
     # datasets = ["Brain"]
     colors = [1, 2, 3, 4, 5, 8, 16, 32]
@@ -24,11 +29,19 @@ function LoECC_Greedy_Compare()
         k = round.(Int64,maximum(EdgeColors))
         for j = 1:length(colors)
             b = colors[j]
-            mat = matread("/scratch/tmp/crane/overlapping-ecc/LoECC/"*dataset*"_b"*string(b)*"_results.mat")
+            mat = matread("Output/LoECC/"*dataset*"_b"*string(b)*"_results.mat")
             lpval = round(mat["bicrit_LPval"], digits=2)
             bicrit_mistakes = mat["bicrit_mistakes"]
-            bicrit_c = convertToArray(mat["bicrit_c"], n, k)
-            greedy_c = convertToArray(mat["greedy_c"], n, k)
+            # bicrit_c = convertToArray(mat["bicrit_c"], n, k)
+            # greedy_c = convertToArray(mat["greedy_c"], n, k)
+
+            # recompute the greedy clustering (only takes seconds)
+            greedy_c = GreedyLocal(EdgeList, EdgeColors, n, k, b)
+
+            # use the optimal LP variables to recompute the rounded clustering
+            bicrit_LPval = mat["bicrit_LPval"]
+            bicrit_X = mat["bicrit_X"]
+            bicrit_c, round_score, round_ratio, budget_score, budget_ratio = LoECCBicriteriaRound(EdgeList, EdgeColors, bicrit_X, bicrit_LPval, b, 0.5)
 
             bicrit_useless_count, bicrit_useless_per_node, bicrit_useless = get_useless_assignments(EdgeList, EdgeColors, bicrit_c)
             greedy_useless_count, greedy_useless_per_node, greedy_useless = get_useless_assignments(EdgeList, EdgeColors, greedy_c)
@@ -67,6 +80,68 @@ end
 
 function GoECC_Greedy_Compare()
 
+    println("GO-ECC")
+
+    # datasets = ["Brain", "MAG-10", "Cooking", "DAWN", "Walmart-Trips", "Trivago"]
+    datasets = ["Trivago"]
+    # budgets = [0.0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]
+    budgets = [1, 1.5, 2, 2.5, 3, 3.5, 4]
+    for i = 1:length(datasets)
+        dataset = datasets[i]
+        println("DATASET: "*dataset*"")
+        data = load("../data/JLD_Files/"*dataset*".jld")
+        EdgeColors = data["EdgeColors"]
+        EdgeList = data["EdgeList"]
+        n = data["n"]
+        M = length(EdgeColors)
+        msize = MaxHyperedgeSize(EdgeList)
+        k = round.(Int64,maximum(EdgeColors))
+
+        for j = 1:length(budgets)
+            budget = Int64(floor(n*budgets[j]))
+            bstring = string(budgets[j])
+            mat = matread("Output/GoECC/"*dataset*"_b"*bstring*"_results.mat")
+
+            # recompute the greedy clustering (only takes seconds)
+            greedy_c = GreedyGlobal(EdgeList, EdgeColors, n, k, budget)
+
+            # we only have the LP variables, so recompute the rounded clustering
+            X = mat["x"]
+            Z = mat["Z"]
+            LPval = mat["LPval"]
+            bicrit_c, round_score, round_ratio, budget_score, budget_ratio = GoECCRound(EdgeList, EdgeColors, X, Z, LPval, budget)
+
+            bicrit_useless_count, bicrit_useless_per_node, bicrit_useless = get_useless_assignments(EdgeList, EdgeColors, bicrit_c)
+            greedy_useless_count, greedy_useless_per_node, greedy_useless = get_useless_assignments(EdgeList, EdgeColors, greedy_c)
+
+            bicrit_unused_count, bicrit_unused_list, bicrit_sat_per_node = get_unused_nodes(EdgeList, EdgeColors, bicrit_c)
+            greedy_unused_count, greedy_unused_list, greedy_sat_per_node = get_unused_nodes(EdgeList, EdgeColors, greedy_c)
+            
+            LPminusG, GminusLP, LPsymdiffG = compare_clusterings(EdgeList, EdgeColors, bicrit_c, greedy_c)
+            LPminusG_size = length(LPminusG)
+            GminusLP_size = length(GminusLP)
+            symdiff_size = length(LPsymdiffG)
+            GminusLP_ratio = GminusLP_size / symdiff_size
+
+            greedy_extra_unused = greedy_unused_count - bicrit_unused_count
+            greedy_extra_useless = greedy_useless_count - bicrit_useless_count
+
+            println("budget = $bstring...")
+            println("   bicrit_useless: $bicrit_useless_count, greedy_useless: $greedy_useless_count, greedy_extra_useless: $greedy_extra_useless")
+            println("   bicrit_unused: $bicrit_unused_count, greedy_unused_count: $greedy_unused_count, greedy_extra_unused: $greedy_extra_unused")
+            println("   bicrit - greedy: $LPminusG_size, greedy - bicrit: $GminusLP_size, symdiff: $symdiff_size  greedy_ratio: $GminusLP_ratio")
+
+            matwrite("Output/stats/goecc"*dataset*"_b"*bstring*"_greedycompare.mat", Dict(
+                "bicrit_useless"=>bicrit_useless_count,"bicrit_unused"=>bicrit_unused_count,
+                "greedy_useless"=>greedy_useless_count,"greedy_unused"=>greedy_unused_count,
+                "LPminusG"=>LPminusG_size,"GminusLP"=>GminusLP_size,"symdiff"=>symdiff_size,
+            ))
+        end
+    end
+
+
+
 end
 
-LoECC_Greedy_Compare()
+# LoECC_Greedy_Compare()
+GoECC_Greedy_Compare()
